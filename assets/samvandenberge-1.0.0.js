ac1d4b267454b6853364cc44dca233f6d35aac37
Sam Van Den Berge <van.den.berge.sam@gmail.com>
2015-05-03 21:53:47 +0200

    fixes

--- assets/samvandenberge-1.0.0.js
+++ assets/samvandenberge-1.0.0.js
@@ -2,6 +2,6 @@
  * SamVanDenBerge - v0.1.0 - 2015-05-03
  * SamVanDenBerge
  * Source code for Sam Van Den Berge website
- * http://samvandenberge.be.net
+ * samvandenberge.be
  * Copyright 2015.  licensed.
  */

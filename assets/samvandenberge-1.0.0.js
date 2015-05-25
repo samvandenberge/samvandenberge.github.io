bfe3d07b54473be3ba60a7a407285c734621ef53
samvandenberge <van.den.berge.sam@gmail.com>
2015-05-25 19:25:39 +0200

    Small fixes

--- assets/samvandenberge-1.0.0.js
+++ assets/samvandenberge-1.0.0.js
@@ -1,5 +1,5 @@
 /*!
- * personal-website - v0.1.0 - 2015-05-23
+ * personal-website - v0.1.0 - 2015-05-25
  * Sam Van Den Berge
  * Source code for Sam Van Den Berge website
  * www.samvandenberge.be
